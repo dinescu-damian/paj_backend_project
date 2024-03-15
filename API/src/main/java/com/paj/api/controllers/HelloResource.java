495a82afcaf45f136b990f8ea26cb27e080e483a
Dragos Costache <dcostache01@gmail.com>
2024-03-15 14:36:04 +0200

    Created a new package for the controllers

--- API/src/main/java/com/paj/api/controllers/HelloResource.java
+++ API/src/main/java/com/paj/api/controllers/HelloResource.java
@@ -1,11 +1,13 @@
-package com.paj.api;
+package com.paj.api.controllers;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
+
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
     @Produces("text/plain")
     public String hello() {
